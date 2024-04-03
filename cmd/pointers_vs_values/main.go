7b767de3497e242e0f82ad84055202f016d5e2aa
Tobiah Rex <tobiah.rex@gmail.com>
2024-04-02 17:06:52 -0700

    remove pointers_vs_structs

--- cmd/pointers_vs_values/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/TobiahRex/learn_golang/pkg/pointers_vs_values"
-)
-
-func main() {
-	pointers_vs_values.PointersVsValues()
-}
--- pkg/pointers_vs_values/pointers_vs_values.go
+++
@@ -1,9 +0,0 @@
-package pointers_vs_values
-
-import (
-	"fmt"
-)
-
-func PointersVsValues() {
-	fmt.Println("Pointers vs Values")
-}
